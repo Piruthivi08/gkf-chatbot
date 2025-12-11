9e90085cf62cfba07b01fa7a31d07d0608fca18c
Piruthiviraj08 <piruthiviraj0f8@gmail.com>
2025-12-11 12:35:26 +0530

    deploy once in 11/12

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
 app.use(
   cors({
     origin: [
@@ -22,7 +21,7 @@ app.use(
     ],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
-    
+
     credentials: true,
   })
 );
